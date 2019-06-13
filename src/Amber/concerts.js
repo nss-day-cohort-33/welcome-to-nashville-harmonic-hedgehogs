c255ea89bc525c37f01951e8aacfe09622a614de
Amber Gooch <agooch91@gmail.com>
2019-06-12 22:56:17 -0500

    Wrote function for add/remove buttons

--- src/Amber/concerts.js
+++ src/Amber/concerts.js
@@ -1,7 +1,6 @@
 const concertsInput = document.querySelector("#concerts-search")
 const concertsButton = document.querySelector("#concerts-button")
 
-// function getConcerts () 
 concertsButton.addEventListener("click", () => {
     console.log(concertsButton.value)
     fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&keyword=${concertsInput.value}&apikey=${app_keys[3].app_key}`)
@@ -9,10 +8,10 @@ concertsButton.addEventListener("click", () => {
         .then(concertData => {
            
             concertData._embedded.events.forEach(concert => {
-                console.log(concertData)
-                console.log(concert.name)
-                console.log(concert.dates.start.localDate)
-                console.log(concert.images[0].url)
+                // console.log(concertData)
+                // console.log(concert.name)
+                // console.log(concert.dates.start.localDate)
+                // console.log(concert.images[0].url)
                 concert._embedded.venues.forEach(venues => {
                     console.log(venues.name)
                     
@@ -27,30 +26,49 @@ concertsButton.addEventListener("click", () => {
         })
 })
 
-
+let num = 0;
 
 function createConcertResults(concertObject) {
+    num++
     return `
-    <div>
+    <article id="card-${num}">
         <h3>${concertObject.name}</h3>
-        <img src="${concertObject.images[0].url}">
+        <img src="${concertObject.images[0].url}" width= "250px">
         <p>${concertObject.dates.start.localDate}</p>
         <p>${concertObject._embedded.venues[0].name}</p>
-         <div>
-            <button id="save-${num}">Save</button>
-        </div>
-    </div>
+         
+        <button id="add-${num}">add</button>
+        <button class="hide" id="remove-${num}">remove</button>
+        
+    </article>
     `
 }
 
 function printConcertResultsToDom (concert) {
-    document.querySelector("#results").innerHTML += createConcertResults(concert)
+    document.querySelector("#concert-results").innerHTML += createConcertResults(concert)
 }
 
-function saveResultElements () {
+function resultElementHandler () {
+    console.log(event.target.id)
+    const targetButtonIdName = event.target.id.split("-")[0]
     const targetButtonIdNumber = event.target.id.split("-")[1]
-    const card = document.getElementById(`card--${targetButton}`)
-    div.removeChild(card)
+
+    const card = document.getElementById(`card-${targetButtonIdNumber}`)
+    const addButton = document.getElementById(`add-${targetButtonIdNumber}`)
+    const removeButton = document.getElementById(`remove-${targetButtonIdNumber}`)
+    
+    if (targetButtonIdName === "add") {
+        document.querySelector("#concert-itinerary").appendChild(card)
+        addButton.classList.add("hide") 
+        removeButton.classList.remove("hide") 
+       
+    } else if (targetButtonIdName === "remove") {
+        document.querySelector("#concert-results").prepend(card)
+        addButton.classList.remove("hide")
+        removeButton.classList.add("hide") 
+    }
+
 }
 
-// getConcerts()
+document.querySelector("#concert-results").addEventListener("click", resultElementHandler)
+document.querySelector("#concert-itinerary").addEventListener("click", resultElementHandler)
